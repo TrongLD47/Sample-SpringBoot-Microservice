187708144364a4fc3d2d8eaefbb060ce67b1f405
trong.le-duc <trong.le-duc@banvien.com.vn>
2023-09-12 16:57:44 +0700

    create sample project using java spring boots & Microservice

---
+++ department-service/src/main/java/com/dailycodebuffer/department/service/DepartmentService.java
@@ -0,0 +1,27 @@
+package com.dailycodebuffer.department.service;
+
+import com.dailycodebuffer.department.entity.Department;
+import com.dailycodebuffer.department.respository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Service
+@Slf4j
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department department) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.save(department);
+    }
+
+    public Department findDepartmentById(Long departmentId) {
+        log.info("Inside findDepartmentById of DepartmentService");
+        return departmentRepository.findByDepartmentId(departmentId);
+    }
+
+}
---
+++ user-service/src/main/java/com/dailycodebuffer/user/service/UserService.java
@@ -0,0 +1,44 @@
+package com.dailycodebuffer.user.service;
+
+import com.dailycodebuffer.user.VO.Department;
+import com.dailycodebuffer.user.VO.ResponseTemplateVO;
+import com.dailycodebuffer.user.entity.User;
+import com.dailycodebuffer.user.repository.UserRepository;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.swing.text.html.parser.Parser;
+
+@Service
+@Slf4j
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public User saveUser(User user) {
+        log.info("Inside saveUser of UserService");
+        return userRepository.save(user);
+    }
+
+    public ResponseTemplateVO getUserWithDepartment(Long userId) {
+        log.info("Inside getUserWithDepartment of UserService");
+        ResponseTemplateVO vo = new ResponseTemplateVO();
+//        User user = userRepository.findByUserId(userId);
+        User user = new User(1L, "Trong", "Le", "letrong191120@gmail.com", 1L);
+
+        Department department =
+                restTemplate.getForObject("http://DEPARTMENT-SERVICE/departments/" + user.getDepartmentId(),
+                        Department.class);
+        vo.setUser(user);
+        vo.setDepartment(department);
+
+        return vo;
+    }
+}
