c9b2ec2e0cbb241488862d679a7a0f51d9f0cf50
trong.le-duc <trong.le-duc@banvien.com.vn>
2023-09-18 13:41:25 +0700

    change config server to using DB

---
+++ Database/schema.sql
@@ -0,0 +1,13 @@
+create table properties
+(
+    PropertyId  BIGSERIAL PRIMARY KEY,
+    Application CHARACTER VARYING(100),
+    Profile     CHARACTER varying(100),
+    Label       CHARACTER varying(100),
+    Key         CHARACTER VARYING(100) NOT NULL,
+    Value       text                   NOT NULL,
+    Description CHARACTER VARYING(1000),
+    CreatedDate TIMESTAMP              NOT NULL DEFAULT NOW(),
+    UpdatedDate TIMESTAMP                       DEFAULT NOW(),
+    UNIQUE (Key, Application, Profile)
+);
